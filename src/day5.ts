3a804f36bfa3e0b3d52691e9c1b610b3288190e8
Tim Huddle <blazhen88@gmail.com>
2020-12-06 17:10:02 -0500

    day5 is actualy just binary

--- src/day5.ts
+++ src/day5.ts
@@ -18,30 +18,10 @@ export function part2 (input: string): number {
   return ids[0] - 1
 }
 
-function prepareInput (input: string): string[][] {
-  return input.trim().split('\n').map(s => s.trim().split(''))
+function prepareInput (input: string): string[] {
+  return input.trim().split('\n').map(s => s.trim())
 }
 
-function findSeatId (boardingPass: string[]): number {
-  let rowArray = new Array(128).fill(null).map((_, i) => i)
-  let colArray = new Array(8).fill(null).map((_, i) => i)
-
-  for (const command of boardingPass) {
-    switch (command) {
-      case 'F':
-        rowArray = rowArray.slice(0, rowArray.length / 2)
-        break
-      case 'B':
-        rowArray = rowArray.slice(rowArray.length / 2)
-        break
-      case 'L':
-        colArray = colArray.slice(0, colArray.length / 2)
-        break
-      case 'R':
-        colArray = colArray.slice(colArray.length / 2)
-        break
-    }
-  }
-
-  return rowArray[0] * 8 + colArray[0]
+function findSeatId (boardingPass: string): number {
+  return Number.parseInt(boardingPass.replace(/(F|L)/g, '0').replace(/(B|R)/g, '1'), 2)
 }
